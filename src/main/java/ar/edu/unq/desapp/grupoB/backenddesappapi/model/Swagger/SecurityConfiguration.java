8424965ba1bc04e85a5b563109c055649cf45a66
MatiasCar <matiascardozo2795@gmail.com>
2022-04-12 21:29:39 -0300

    feat: Agrego clase segurity configuration para swagger

---
+++ src/main/java/ar/edu/unq/desapp/grupoB/backenddesappapi/model/Swagger/SecurityConfiguration.java
@@ -0,0 +1,26 @@
+package ar.edu.unq.desapp.grupoB.backenddesappapi.model.Swagger;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/v2/api-docs",
+                "/configuration/ui",
+                "/swagger-resources/**",
+                "/configuration/security",
+                "/swagger-ui/**",
+                "/swagger-ui",
+                "/swagger-ui.html",
+                "/swagger/**",
+                "/webjars/**");
+    }
+}

aaeeb5ac22e2e00eb796acdae8aa8c5a9fbc4b03
MatiasCar <matiascardozo2795@gmail.com>
2022-05-04 14:24:42 -0300

    Feat: Agrego validacion de formato de email y nuevos atributos a clase user

--- src/main/java/ar/edu/unq/desapp/grupoB/backenddesappapi/model/User.java
+++ src/main/java/ar/edu/unq/desapp/grupoB/backenddesappapi/model/User.java
@@ -1,20 +1,33 @@
 package ar.edu.unq.desapp.grupoB.backenddesappapi.model;
 
+import ar.edu.unq.desapp.grupoB.backenddesappapi.model.Utils.Exceptions.WrongEmailFormatException;
+
 import javax.persistence.*;
+
+import static ar.edu.unq.desapp.grupoB.backenddesappapi.model.Utils.EmailFormatAuthentication.patternMatches;
 
 @Entity
 @Table(name="user_table")
 public class User {
 
-    public User(Integer id, String name, String lastname, String email, String address, String password, String cvu, String userWallet) {
+    public User(Integer id, String name, String lastname, String email, String address, String password, String cvu, String userWallet) throws Exception {
         this.id = id;
         this.name = name;
         this.lastname = lastname;
-        this.email = email;
         this.address = address;
         this.password = password;
         this.cvu = cvu;
         this.userWallet = userWallet;
+        this.reputation = 0;
+        this.successfulOperations = 0;
+        this.totalOperations = 0;
+
+        if(patternMatches(email)){
+            this.email = email;
+        }
+        else{
+            throw new WrongEmailFormatException("The email entered does not comply with the format");
+        }
     }
 
     public User(){  }
@@ -42,6 +55,39 @@ public class User {
 
     @Column(name="user_wallet",nullable = false, length = 8)
     private String userWallet;
+
+    @Column(name="user_reputation", nullable = false)
+    private Integer reputation;
+
+    @Column(name = "successful_operations", nullable = false)
+    private Integer successfulOperations;
+
+    @Column(name = "total_operations",nullable = false)
+    private Integer totalOperations;
+
+    public Integer getTotalOperations() {
+        return totalOperations;
+    }
+
+    public void setTotalOperations(Integer totalOperations) {
+        this.totalOperations = totalOperations;
+    }
+
+    public Integer getSuccessfulOperations() {
+        return successfulOperations;
+    }
+
+    public void setSuccessfulOperations(Integer successfulOperations) {
+        this.successfulOperations = successfulOperations;
+    }
+
+    public Integer getReputation() {
+        return reputation;
+    }
+
+    public void setReputation(Integer reputation) {
+        this.reputation = reputation;
+    }
 
     public Integer getId() {
         return id;

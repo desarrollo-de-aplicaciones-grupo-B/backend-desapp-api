1262b7ac50f9b42c4791fe6299cd65176ccc4689
franarci <fran.arciniega96@gmail.com>
2022-06-25 20:37:36 -0300

    Add comment

--- src/main/java/ar/edu/unq/desapp/grupoB/backenddesappapi/services/UserDetailsService.java
+++ src/main/java/ar/edu/unq/desapp/grupoB/backenddesappapi/services/UserDetailsService.java
@@ -3,7 +3,7 @@ package ar.edu.unq.desapp.grupoB.backenddesappapi.services;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
-public interface UserDetailsService {
+public interface UserDetailsService { // interface that loads user-specific data
     UserDetails loadUserByUsername(String username)
             throws UsernameNotFoundException;
 }

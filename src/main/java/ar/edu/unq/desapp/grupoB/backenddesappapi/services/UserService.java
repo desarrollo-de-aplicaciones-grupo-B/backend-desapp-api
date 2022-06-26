15288ebfc53f0076276bcff0dd93e08c51f6d6d0
franarci <fran.arciniega96@gmail.com>
2022-06-26 01:58:11 -0300

    methods in UserService

--- src/main/java/ar/edu/unq/desapp/grupoB/backenddesappapi/services/UserService.java
+++ src/main/java/ar/edu/unq/desapp/grupoB/backenddesappapi/services/UserService.java
@@ -1,11 +1,14 @@
 package ar.edu.unq.desapp.grupoB.backenddesappapi.services;
 
+import ar.edu.unq.desapp.grupoB.backenddesappapi.model.Trading;
 import ar.edu.unq.desapp.grupoB.backenddesappapi.model.User;
 import ar.edu.unq.desapp.grupoB.backenddesappapi.repositories.IUserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
 import java.util.List;
 import java.util.Optional;
 
@@ -50,4 +53,60 @@ public class UserService {
     @Transactional
     public User findUserByName(String username) { return this.userRepository.findUserByName(username);}
 
+    @Transactional
+    public void openTrading(Integer userId, Integer cryptoId, Double cryptoAmount, Double cotization, Double operationAmount){
+        tradingService().save(new Trading(cryptoId,cryptoAmount,cotization, operationAmount, userId));
+    }
+
+    @Transactional
+    public void buy(Integer userId, Integer tradingId){
+        Trading trading = tradingService().findByID(tradingId);
+        trading.setBuyerId(userId);
+    }
+
+    @Transactional
+    public void cancel(Integer userId,Integer tradingId){
+        Trading trading = tradingService().findByID(tradingId);
+        User canceller = findByID(userId);
+        if(trading.getBuyerId().equals(userId)){
+            canceller.penalize();
+        } else
+        if(trading.getSellerId().equals(userId) && trading.getBuyerId() != null){ //If there is not a buyer yet the seller wont get penalized
+            canceller.penalize();
+        }
+        tradingService().deleteById(trading.getIdOperation());
+    }
+
+    @Transactional
+    public void confirmTransfer(Integer userId,Integer tradingId){
+        Trading trading = tradingService().findByID(tradingId);
+        trading.confirmTransfer(userId);
+        tradingService().updateTrading(trading);
+    }
+
+    @Transactional
+    public void confirmReception(Integer userId,Integer tradingId){
+        Trading trading = tradingService().findByID(tradingId);
+        LocalDateTime confirmationDate = LocalDateTime.now();
+        if(trading.getSellerId().equals(userId)) {
+            if(cotizationIsOK(trading.getCryptoId(), trading.getCotization())){
+                Long timeDifference = ChronoUnit.MINUTES.between(trading.getCreationDate(), confirmationDate);
+                User seller = findByID(userId);
+                User buyer = findByID(trading.getBuyerId());
+                seller.successfulTrading(timeDifference);
+                buyer.successfulTrading(timeDifference);
+            } else {
+                tradingService().deleteById(trading.getIdOperation());
+            }
+        }
+    }
+    public boolean cotizationIsOK(Integer cryptoId, Double cotization){
+        return cotizationService().cotizationIsOK(cryptoId, cotization);
+    }
+    public TradingService tradingService(){
+        return new TradingService();
+    }
+    public CotizationService cotizationService(){
+        return new CotizationService();
+    }
 }

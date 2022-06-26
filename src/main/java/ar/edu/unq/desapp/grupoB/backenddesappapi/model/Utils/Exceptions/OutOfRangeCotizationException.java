7f6654f230fc1151d5bf9e18ca076c9bf8f6bba1
franarci <fran.arciniega96@gmail.com>
2022-06-26 12:52:15 -0300

    Out of range cotization exception

---
+++ src/main/java/ar/edu/unq/desapp/grupoB/backenddesappapi/model/Utils/Exceptions/OutOfRangeCotizationException.java
@@ -0,0 +1,5 @@
+package ar.edu.unq.desapp.grupoB.backenddesappapi.model.Utils.Exceptions;
+
+public class OutOfRangeCotizationException extends Exception{
+    public OutOfRangeCotizationException(String errorMessage) { super(errorMessage); }
+}

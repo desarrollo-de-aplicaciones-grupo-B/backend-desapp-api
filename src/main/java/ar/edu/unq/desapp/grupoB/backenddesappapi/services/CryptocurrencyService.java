c867b2802e0033fd21f53de72591f3c042b91249
franarci <fran.arciniega96@gmail.com>
2022-06-29 11:49:42 -0300

    CryptoService save() and findById()

--- src/main/java/ar/edu/unq/desapp/grupoB/backenddesappapi/services/CryptocurrencyService.java
+++ src/main/java/ar/edu/unq/desapp/grupoB/backenddesappapi/services/CryptocurrencyService.java
@@ -1,12 +1,12 @@
 package ar.edu.unq.desapp.grupoB.backenddesappapi.services;
 import ar.edu.unq.desapp.grupoB.backenddesappapi.model.Cryptocurrency;
-import ar.edu.unq.desapp.grupoB.backenddesappapi.model.User;
 import ar.edu.unq.desapp.grupoB.backenddesappapi.repositories.ICryptocurrencyRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
 import java.util.List;
+import java.util.Optional;
 
 
 @Service
@@ -19,5 +19,13 @@ public class CryptocurrencyService {
     public List<Cryptocurrency> getAllCryptocurrencyPrices() {
         return this.cryptocurrencyRepository.findAll();
     }
+
+    @Transactional
+    public void save(Cryptocurrency c){
+        this.cryptocurrencyRepository.save(c);
+    }
+
+    @Transactional
+    public Optional<Cryptocurrency> findById(Integer id){ return this.cryptocurrencyRepository.findById(id); }
 
 }

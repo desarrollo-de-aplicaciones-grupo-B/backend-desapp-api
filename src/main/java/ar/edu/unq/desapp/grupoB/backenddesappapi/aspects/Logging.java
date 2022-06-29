cd7664387aecdecce95191eeff84b4ad0411e268
franarci <fran.arciniega96@gmail.com>
2022-06-29 14:12:05 -0300

    Fixed Logger

--- src/main/java/ar/edu/unq/desapp/grupoB/backenddesappapi/aspects/Logging.java
+++ src/main/java/ar/edu/unq/desapp/grupoB/backenddesappapi/aspects/Logging.java
@@ -1,11 +1,11 @@
 package ar.edu.unq.desapp.grupoB.backenddesappapi.aspects;
 
+import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
 import org.springframework.context.annotation.Configuration;
 
 import java.text.SimpleDateFormat;
@@ -20,17 +20,18 @@ public class Logging {
 
     private static final Logger logger = LogManager.getLogger(Logging.class);
 
-    @Around("within(@org.springframework.web.bind.annotation.RestController *)")
+    @Around("within (@org.springframework.web.bind.annotation.RestController *)")
     public Object logAround(ProceedingJoinPoint joinPoint) throws Throwable {
-        logger.debug("----------------------------------------------------------------------------------------------------------------------------------------------------------------");
-        logger.debug("Request for {}.{}() with argument[s] = {}", joinPoint.getSignature().getDeclaringTypeName(),
-                                                                  joinPoint.getSignature().getName(), Arrays.toString(joinPoint.getArgs()));
         Instant start = Instant.now();
         Object returnValue= joinPoint.proceed();
         Instant finish = Instant.now();
         long timeElapsed = Duration.between(start,finish).toMillis();
-        logger.debug("Time taken: "+new SimpleDateFormat("mm:ss:SSS").format(new Date(timeElapsed)));
-        logger.debug("----------------------------------------------------------------------------------------------------------------------------------------------------------------");
+
+        logger.log(Level.INFO,"----------------------------------------------------------------------------------------------------------------------------------------------------------------");
+        logger.log(Level.INFO,"Request for {}.{}() with argument[s] = {}", joinPoint.getSignature().getDeclaringTypeName(),
+                                                                  joinPoint.getSignature().getName(), Arrays.toString(joinPoint.getArgs()));
+        logger.log(Level.INFO, "Time taken: "+new SimpleDateFormat("mm:ss:SSS").format(new Date(timeElapsed)));
+        logger.log(Level.INFO,"----------------------------------------------------------------------------------------------------------------------------------------------------------------");
         return returnValue;
     }
 }

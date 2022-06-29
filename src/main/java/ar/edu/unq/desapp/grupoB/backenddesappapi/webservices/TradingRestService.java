103aa38841e13893840af1feb9bba60b25ace4c6
franarci <fran.arciniega96@gmail.com>
2022-06-29 10:55:58 -0300

    getAllTradingUserDTOs

--- src/main/java/ar/edu/unq/desapp/grupoB/backenddesappapi/webservices/TradingRestService.java
+++ src/main/java/ar/edu/unq/desapp/grupoB/backenddesappapi/webservices/TradingRestService.java
@@ -16,11 +16,6 @@ public class TradingRestService {
     @Autowired
     private TradingService tradingService;
 
-    @GetMapping("/{userId}")
-    public List<TradingUserDTO> getUserTradings(@PathVariable("userId") Integer userId){
-        return tradingService.getAllTradingUserDTO(userId);
-    }
-
     @PostMapping
     public void save(@RequestBody Trading trading){
         tradingService.save(trading);
@@ -33,5 +28,10 @@ public class TradingRestService {
 
     @PostMapping(value = "/{id}")
     public void update(@RequestBody Trading trading) { tradingService.updateTrading(trading);
+    }
+
+    @GetMapping("/{userId}")
+    public List<TradingUserDTO> getUserTradings(@PathVariable("userId") Integer userId){
+        return tradingService.getAllTradingUserDTO(userId);
     }
 }

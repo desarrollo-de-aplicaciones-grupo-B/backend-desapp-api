98093e0d458941f217249aa6e8bea4e8bdedb00b
franarci <fran.arciniega96@gmail.com>
2022-07-23 20:45:29 -0300

    Correct price margin check

--- src/main/java/ar/edu/unq/desapp/grupoB/backenddesappapi/services/CotizationService.java
+++ src/main/java/ar/edu/unq/desapp/grupoB/backenddesappapi/services/CotizationService.java
@@ -1,12 +1,12 @@
 package ar.edu.unq.desapp.grupoB.backenddesappapi.services;
 
 import ar.edu.unq.desapp.grupoB.backenddesappapi.model.Cotization;
-import ar.edu.unq.desapp.grupoB.backenddesappapi.model.User;
+import ar.edu.unq.desapp.grupoB.backenddesappapi.model.Cryptocurrency;
 import ar.edu.unq.desapp.grupoB.backenddesappapi.model.Utils.Exceptions.OutOfRangeCotizationException;
 import ar.edu.unq.desapp.grupoB.backenddesappapi.repositories.ICotizationRepository;
+import ar.edu.unq.desapp.grupoB.backenddesappapi.repositories.ICryptocurrencyRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.RequestBody;
 
 import javax.transaction.Transactional;
 import java.util.List;
@@ -17,6 +17,9 @@ public class CotizationService {
 
     @Autowired
     private ICotizationRepository cotizationRepository;
+
+    @Autowired
+    private ICryptocurrencyRepository cryptoRepository;
 
     @Transactional
     public void save(Cotization cotization){
@@ -46,18 +49,17 @@ public class CotizationService {
         return this.cotizationRepository.findLast24HoursCotizations(name);
     }
     public void checkPriceMargin(Integer cryptoId, Double cotization) throws OutOfRangeCotizationException {
-        Optional<Cotization> systemCotization = findById(cryptoId);
-        if(systemCotization.isPresent()){
-            Double margin = 5 * systemCotization.get().getPriceCotization() / 100;
-            Double min = systemCotization.get().getPriceCotization() - margin;
-            Double max = systemCotization.get().getPriceCotization() + margin;
-            if(cotization < min){
-                throw new OutOfRangeCotizationException("The price is below system cotization by more than 5%");
-            }
-            if(cotization > max){
-                throw new OutOfRangeCotizationException("The price is above system cotization by more than 5%");
-            }
-        } //TODO throw error not found
+        Optional<Cryptocurrency> crypto = cryptoRepository.findById(cryptoId);
+        Optional<Cotization> systemCotization = Optional.ofNullable(findLastCotization(crypto.get().getNomenclature()));
+        Double margin = 5 * systemCotization.get().getPriceCotization() / 100;
+        Double min = systemCotization.get().getPriceCotization() - margin;
+        Double max = systemCotization.get().getPriceCotization() + margin;
+        if(cotization < min){
+            throw new OutOfRangeCotizationException("The price is below system cotization by more than 5%");
+        }
+        if(cotization > max){
+            throw new OutOfRangeCotizationException("The price is above system cotization by more than 5%");
+        }
     }
 
     @Transactional

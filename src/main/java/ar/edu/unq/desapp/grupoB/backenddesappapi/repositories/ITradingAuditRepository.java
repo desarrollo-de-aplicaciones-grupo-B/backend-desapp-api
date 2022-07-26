499d45a473bc635e18f846059d56d9d444c5f3ca
franarci <fran.arciniega96@gmail.com>
2022-07-25 22:16:29 -0300

    Architecture tests

--- src/main/java/ar/edu/unq/desapp/grupoB/backenddesappapi/repositories/ITradingAuditRepository.java
+++ src/main/java/ar/edu/unq/desapp/grupoB/backenddesappapi/repositories/ITradingAuditRepository.java
@@ -1,12 +1,13 @@
 package ar.edu.unq.desapp.grupoB.backenddesappapi.repositories;
 
-import ar.edu.unq.desapp.grupoB.backenddesappapi.model.Trading;
 import ar.edu.unq.desapp.grupoB.backenddesappapi.model.TradingAudit;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface ITradingAuditRepository extends CrudRepository<TradingAudit, Integer> {
 
     Optional<TradingAudit> findById(Integer id);

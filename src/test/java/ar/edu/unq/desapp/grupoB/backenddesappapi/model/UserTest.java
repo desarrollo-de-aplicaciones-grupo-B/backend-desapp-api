701923a652e95f3326bc696555d5a44f1438aff2
franarci <fran.arciniega96@gmail.com>
2022-10-01 19:59:46 -0300

    cambio nombres y agrego AfterEach()

--- src/test/java/ar/edu/unq/desapp/grupoB/backenddesappapi/model/UserTest.java
+++ src/test/java/ar/edu/unq/desapp/grupoB/backenddesappapi/model/UserTest.java
@@ -1,13 +1,11 @@
-package ar.edu.unq.desapp.grupoB.backenddesappapi.model.Utils;
+package ar.edu.unq.desapp.grupoB.backenddesappapi.model;
 
-import ar.edu.unq.desapp.grupoB.backenddesappapi.model.Reputation;
-import ar.edu.unq.desapp.grupoB.backenddesappapi.model.User;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.mockito.MockedStatic;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.mockStatic;
 
 public class UserTest {
     User pepe;
@@ -15,24 +13,32 @@ public class UserTest {
     User goodUser;
 
     MockedStatic<Reputation> reputation;
-    private User anyUser() throws Exception {
-        return new User(43, "asdsaf",
+    private User anyUser() {
+        try {
+            return new User(43, "asdsaf",
                     "asda",
                     "any@mail.com",
                     "lakosdksa",
                     "13134",
                     "95431685",
                     "12345678910");
+        } catch (Exception e){
+            return new User();
+        }
     }
 
-   private User goodReputationUser() throws Exception {
-       return new User(87, "good",
-               "user",
-               "good@mail.com",
-               "qweq",
-               "42kjh8df",
-               "87578422",
-               "73465821465");
+   private User goodReputationUser() {
+       try {
+           return new User(87, "good",
+                   "user",
+                   "good@mail.com",
+                   "qweq",
+                   "42kjh8df",
+                   "87578422",
+                   "73465821465");
+       } catch (Exception e) {
+           return new User();
+       }
    }
 
     private Long lessThanHalfHour() {
@@ -42,17 +48,30 @@ public class UserTest {
     private Long moreThanHalfHour(){
         return 45L;
     }
-
+    private Integer nPoints() {
+        return 15;
+    }
+    private Integer nTransactions() {
+        return 1;
+    }
 
     @BeforeEach
-    void setUp() throws Exception {
+     void setUp() {
         pepe = anyUser();
         goodUser = goodReputationUser();
         reputation = mockStatic(Reputation.class);
-        reputation.when(() -> Reputation.calculate(1,15)).thenCallRealMethod();
+        reputation.when(() -> Reputation.calculate(nTransactions(),nPoints())).thenCallRealMethod();
         reputation.when(()-> Reputation.pointsToAdd(lessThanHalfHour())).thenCallRealMethod();
         reputation.when(()-> Reputation.pointsToAdd(moreThanHalfHour())).thenCallRealMethod();
         reputation.when(Reputation::penalizationPoints).thenCallRealMethod();
+    }
+
+
+
+
+    @AfterEach
+    void tearDown(){
+        reputation.close();
     }
 
     @Test
@@ -69,7 +88,7 @@ public class UserTest {
         assertEquals(0.0, pepe.getReputation());
     }
     @Test
-    void whenUserAddsSuccesfulTradingInLessThanHalfHourGets10Points(){
+    void whenUserAddsSuccesfulTradingInLessThanHalfHourGets10PointsAnd1SuccessfulOperation(){
         goodUser.successfulTrading(lessThanHalfHour());
         goodUser.getReputation();
         reputation.verify(
@@ -77,7 +96,7 @@ public class UserTest {
         );
     }
     @Test
-    void whenUserAddsSuccesfulTradingInLMoreThanHalfHourGets5Points(){
+    void whenUserAddsSuccesfulTradingInLMoreThanHalfHourGets5PointsAnd1SuccessfulOperation(){
         goodUser.successfulTrading(moreThanHalfHour());
         goodUser.getReputation();
         reputation.verify(
